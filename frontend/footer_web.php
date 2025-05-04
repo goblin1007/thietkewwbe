0d06728c07a97f16360ef4374fb917ffdd4455f6
Lan Anh <k3ai.lananh@gmail.com>
2025-05-04 18:26:49 +0700

    Thêm CSS nội bộ và sửa lỗi modal không hiển thị

--- frontend/footer_web.php
+++ frontend/footer_web.php
@@ -1,16 +1,21 @@
+<link rel="stylesheet" href="trangchu/assets/css/style.css">
 <footer>
     <section class="footer_contact">
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
                     <div class="footer_contact_text">
-                        <h3>Liên hệ</h3>
-                        <h2>Để được tư vấn<br>chương trình tuyển sinh</h2>
+                        <h3 >Liên hệ</h3>
+                        <h2>
+                            Để được tư vấn
+                            <br>
+                            chương trình tuyển sinh
+                        </h2>
                         <span class="info_contact">
-                            <i class="fa-solid fa-phone-volume info_contact_icon"></i>
+                            <i class="fa-solid fa-phone-volume info_contact_icon" ></i>
                             <span>Điện thoại: (024) 3764 3219</span>
                         </span><br>
-                        <span class="info_contact">
+                        <span class="info_contact ">
                             <i class="fa-solid fa-phone-volume info_contact_icon"></i>
                             <span>Fax: (024) 37643228</span>
                         </span><br>
@@ -24,7 +29,7 @@
                 <div class="col-md-6">
                     <form action="" class="footer_contact_form">
                         <div class="row-inp">
-                            <input type="text" placeholder=" " class="input_contact_form" name="fullname" id="fullname_footer">
+                            <input type="text" placeholder=" "  class="input_contact_form" name="fullname" id="fullname_footer">
                             <label>Họ và Tên<b>*</b></label>
                         </div>
                         <div class="contact_tel_email">
@@ -35,108 +40,193 @@
                             <div class="row-inp row-inp-50">
                                 <input type="text" placeholder=" " class="input_contact_form" name="email" id="email_footer">
                                 <label>Email<b>*</b></label>
-                            </div>
+                        </div>
                         </div>
                         <div class="row-inp">
-                            <select name="train_program" id="train_program_footer" class="select_contact_form">
-                                <option value="">Chương trình</option>
-                                <option value="Đại học Chính quy">Đại học Chính quy</option>
-                                <option value="Thạc sĩ">Thạc sĩ</option>
-                                <option value="Tiến sĩ">Tiến sĩ</option>
-                                <option value="Liên kết đào tạo quốc tế">Liên kết đào tạo quốc tế</option>
-                                <option value="Đào tạo bằng 2 & Vừa làm vừa học">Đào tạo bằng 2 & Vừa làm vừa học</option>
-                                <option value="Khoá học ngắn hạn">Khoá học ngắn hạn</option>
+                            <select name="train_program" id="train_program_footer" class="select_contact_form"><option value="">Chương trình</option><option value="85">Đại học Chính quy</option><option value="61">Thạc sĩ</option><option value="90">Tiến sĩ</option><option value="72">Liên kết đào tạo quốc tế</option><option value="73">Đào tạo bằng 2 &amp; Vừa làm vừa học</option><option value="74">Khoá học ngắn hạn</option>
                             </select>
                         </div>
                         <div class="row-inp">
                             <input type="text" placeholder="Câu hỏi của bạn" class="input_contact_form" name="note" id="note_footer">
                         </div>
-                        <div class="btn_submit">
-                            <button type="button" class="btn btn_submit_text" id="submit_contact">GỬI</button>
-                        </div>
+                        <div class="btn_submit ">
+                            <button type="button" class="btn btn_submit_text" id="submit_contact">GỬI</button></div>
                     </form>
                 </div>
             </div>
         </div>
     </section>
+</footer>
+<div class="modal" id="modal">
+    <div class="modal__overlay" id="modal__overlay"  ></div> <!--lop phu--> 
 
-    <div class="footer_social">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="footer_social_container">
-                        <span>
-                            Kết nối với chúng tôi:
-                        </span>
-                        <div class="footer_social_icon">
-                            <a href="https://www.facebook.com/thuongmaiuniversity/">
-                                <img src="assets/img/ic-fb.png" alt="fb">
-                            </a>
-                            <a href="https://www.facebook.com/thuongmaiuniversity/">
-                                <img src="assets/img/ic-mess.png" alt="mess">
-                            </a>
-                            <a href="https://www.youtube.com/channel/UC9-NJM8V8oXewEIfPHica_Q">
-                                <img src="assets/img/ic-ytb.png" alt="ytb">
-                            </a>
-                            <a href="">
-                                <img src="assets/img/ic-in.png" alt="ins">
-                            </a>
-                        </div>
-                    </div>
+    <div class="modal__body">
+        <div class="notice_container">
+            <div class="notice_logo_text">
+                <div class="notice_logo">
+                    <i class="fa-regular fa-circle-xmark notice_logo_icon"></i>
+                </div>
+                <div class="notice_text">
+                    <span>Thông báo</span>
                 </div>
             </div>
+            <div class="notice_content">
+                <span>Vui lòng nhập số điện thoại</span>
+            </div>
+            <button> OK</button>
         </div>
     </div>
+</div>
 
-    <div class="main-footer" style="background-color:rgb(3, 41, 97); color: white;">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-12 d-flex">
-                    <div class="me-3">
-                        <a href="" class="logo-ft">
-                            <img src="assets/img/logo.png" alt="logo_footer">
+<div class="footer_social">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer_social_container">
+                    <span>
+                        Kết nối với chúng tôi:
+                    </span>
+                    <div class="footer_social_icon">
+                        <a href="https://www.facebook.com/thuongmaiuniversity/">
+                            <img src="./assets/img/ic-fb.png" alt="fb">
+                        </a>
+                        <a href="https://www.facebook.com/thuongmaiuniversity/">
+                            <img src="./assets/img/ic-mess.png" alt="mess">                            </a>
+                        <a href="https://www.youtube.com/channel/UC9-NJM8V8oXewEIfPHica_Q">
+                            <img src="./assets/img/ic-ytb.png" alt="ytb">
+                        </a>
+                        <a href="">
+                            <img src="./assets/img/ic-in.png" alt="ins">
                         </a>
                     </div>
-                    <div class="footer_add">
-                        <div class="add-ft">
-                            <h3>Cơ sở 1</h3>
-                            <span>Địa chỉ: 79 Hồ Tùng Mậu,<br>Cầu Giấy, Hà Nội</span><br>
-                            <a href="tel:(024) 3764 3219">Điện thoại: (024) 3764 3219</a><br>
-                            <a href="fax:(024) 37643228">Fax: (024) 37643228</a><br>
-                            <a href="mailto:mail@tmu.edu.vn">Email: mail@tmu.edu.vn</a><br>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-6 col-md-12">
-                    <div class="footer_map" style="height: 200px;">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.423949974385!2d105.7754261!3d21.036643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b5922f1cc3%3A0xe7bcefed820757df!2zNzkgxJAuIEjhu5MgVMO5bmcgT-G7sSwgTWFpIMSQ4buLbmgsIELDrG5oIEThu6sgQ2jDrSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2sus!4v1712815000000"
-                            width="100%"
-                            height="100%"
-                            style="border:0;"
-                            allowfullscreen=""
-                            loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade">
-                        </iframe>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="footer_copyright text-center py-3">
-                        <span id="year"></span> © Bản quyền thuộc về Đại học Thương mại
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-</footer>
+<div class="main-footer" style= "background: #1b4a98">
+    <div class="container">
+        <div class="row">
 
-<script lang="javascript" src="footer.js"></script>
+        <div class="col-lg-6 col-md-12 d-flex">
+            <div class="me-3">
+            <a href="" class="logo-ft">
+                <img src="./assets/img/logo.png" alt="logo_footer" >
+            </a>
+            </div>
+            <div class="footer_add">
+            <div class="add-ft">
+                <h3>Cơ sở 1</h3>
+                <span>Địa chỉ: 79 Hồ Tùng Mậu,<br>Cầu Giấy, Hà Nội</span><br>
+                <a href="tel:(024) 3764 3219">Điện thoại: (024) 3764 3219</a><br>
+                <a href="fax:(024) 37643228">Fax: (024) 37643228</a><br>
+                <a href="mailto:mail@tmu.edu.vn">Email: mail@tmu.edu.vn</a><br>
+            </div>
+            </div>
+        </div>
+    
+        
+        <div class="col-lg-6 col-md-12">
+            <div class="footer_map" style="height: 200px;">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.423949974385!2d105.7754261!3d21.036643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b5922f1cc3%3A0xe7bcefed820757df!2zNzkgxJAuIEjhu5MgVMO5bmcgT-G7sSwgTWFpIMSQ4buLbmgsIELDrG5oIEThu6sgQ2jDrSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2sus!4v1712815000000"
+                width="100%"
+                height="100%"
+                style="border:0;"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+            </div>
+        </div>
+        </div>
+    
+        
+        <div class="footer_copyright text-center">
+        © Bản quyền thuộc về Đại học Thương mại
+        </div>
+    
+    </div>
+    </div>
+
+
+<script >
+const inputs = document.querySelectorAll('.input_contact_form');
+inputs.forEach(input => {
+    const label = input.nextElementSibling;
+
+    input.addEventListener('focus', () => {
+        label.style.display = 'none';
+    });
+
+    input.addEventListener('blur', () => {
+        if (label && input.value === '') {
+            label.style.display = 'block';
+        }
+    });
+});
+
+
+document.querySelectorAll(".modal__overlay, .notice_container > button").forEach(function (el) {
+    el.addEventListener("click", function () {
+    document.querySelector(".modal").style.display = "none";
+    document.querySelector(".modal__overlay").style.display = "none";
+    document.querySelector(".notice_container").style.display = "none";
+})})
+
+
+document.getElementById("submit_contact").addEventListener("click", function () {
+    const fullname = document.getElementById("fullname_footer").value.trim();
+    const phone = document.getElementById("phone_footer").value.trim();
+    const email = document.getElementById("email_footer").value.trim();
+    const modalContent = modal.querySelector(".notice_content span");
+    const phoneRegex = /^(0|\+84)(3|5|7|8|9)[0-9]{8}$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  
+    if (fullname === "") {
+        modalContent.textContent = "Vui lòng nhập Họ và Tên";
+        document.querySelector(".modal").style.display = "flex";
+        document.querySelector(".modal__overlay").style.display = "block";
+         document.querySelector(".notice_container").style.display = "flex";
+        return;
+      }
+    if (phone === "") {
+        modalContent.textContent = "Vui lòng nhập số điện thoại";
+        document.querySelector(".modal").style.display = "flex";
+        document.querySelector(".modal__overlay").style.display = "block";
+         document.querySelector(".notice_container").style.display = "flex";
+        return; 
+      }
+
+      if (!phoneRegex.test(phone)) {
+        modalContent.textContent = "Vui lòng nhập đúng định dạng Số điện thoại";
+        document.querySelector(".modal").style.display = "flex";
+        document.querySelector(".modal__overlay").style.display = "block";
+         document.querySelector(".notice_container").style.display = "flex";
+        return;
+      }
+      if (email === "") {
+        modalContent.textContent = "Vui lòng nhập Email";
+        document.querySelector(".modal").style.display = "flex";
+        document.querySelector(".modal__overlay").style.display = "block";
+         document.querySelector(".notice_container").style.display = "flex";
+        return;
+      }
+      
+      if (!emailRegex.test(email)) {
+        modalContent.textContent = "Vui lòng nhập đúng định dạng Email";
+        document.querySelector(".modal").style.display = "flex";
+        document.querySelector(".modal__overlay").style.display = "block";
+         document.querySelector(".notice_container").style.display = "flex";
+        return;
+      }
+    
+    document.querySelector(".footer_contact_form").submit();
+  });
+
+</script>
 <script src="trangchu.js"></script>
 <script>
     // Set year dynamically
--- frontend/headerweb.php
+++ frontend/headerweb.php
@@ -127,7 +127,67 @@
     </header>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
-<script lang="javascript" src="header.js"></script>
+<script >
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.querySelector('.header_lang_btn');
+    const list = document.querySelector('.header_lang_list');
+
+    // Toggle hiển thị khi click vào nút
+    btn.addEventListener('click', function (event) {
+        event.stopPropagation(); // Ngăn sự kiện click lan ra ngoài
+        list.style.display = (list.style.display === 'flex') ? 'none' : 'flex';
+    });
+
+    // Ẩn khi click ra ngoài danh sách
+    document.addEventListener('click', function (e) {
+        if (!list.contains(e.target) && !btn.contains(e.target)) {
+            list.style.display = 'none';
+        }
+    });
+});
+
+document.querySelector('.introduce_item').onmouseover = function (){
+    document.querySelector('.introduce_item_list').style.display='block';
+}
+document.querySelector('.introduce_item').onmouseout = function (){
+    document.querySelector('.introduce_item_list').style.display='none';
+}
+document.querySelector('.introduce_item_list').onmouseout = function(){
+    document.querySelector('.introduce_item_list').style.display='none';
+}
+
+document.querySelector('.news_item').onmouseover = function (){
+    document.querySelector('.news_item_list').style.display='block';
+}
+document.querySelector('.news_item').onmouseout = function (){
+    document.querySelector('.news_item_list').style.display='none';
+}
+document.querySelector('.dropdown-menu').onmouseout = function(){
+    document.querySelector('.news_item_list').style.display='none';
+}
+
+document.querySelector('.trainning_item').onmouseover = function (){
+    document.querySelector('.trainning_item_list').style.display='block';
+}
+document.querySelector('.trainning_item').onmouseout = function (){
+    document.querySelector('.trainning_item_list').style.display='none';
+}
+document.querySelector('.trainning_item_list').onmouseout = function(){
+    document.querySelector('.trainning_item_list').style.display='none';
+}
+
+document.querySelector(' .admission_item').onmouseover = function (){
+    document.querySelector(' .admission_item_list').style.display='block';
+}
+document.querySelector(' .admission_item').onmouseout = function (){
+    document.querySelector('.admission_item_list').style.display='none';
+}
+document.querySelector('.admission_item_list').onmouseout = function(){
+    document.querySelector('.admission_item_list').style.display='none';
+}
+
+
+</script>
 </body>
 
 </html>
